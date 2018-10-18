e0c7100f86c9333aa8a08de867df9eb74c2c7a2e
Mikel Larreategi <mlarreategi@codesyntax.com>
2018-10-18 23:43:31 +0200

    Preparing release 1.0b1
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.0.0 (unreleased)
+1.0b1 (2018-10-18)
 ------------------
 
 - No changes yet.
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '1.0.0.dev0'
+version = '1.0b1'
 
 
 long_description = '\n\n'.join([
